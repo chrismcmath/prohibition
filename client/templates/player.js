278bb414321795bc76515600d836abb3e5231587
Chris McMath <chrismcmath@gmail.com>
2015-05-31 17:53:45 +0800

    Reorg folder structure

---
+++ client/templates/player.js
@@ -0,0 +1,11 @@
+Template.player.helpers({
+  selected: function () {
+    return Session.equals("selectedPlayer", this._id) ? "selected" : '';
+  }
+});
+
+Template.player.events({
+  'click': function () {
+    Session.set("selectedPlayer", this._id);
+  }
+});
--- libs/common.js
+++ libs/common.js
@@ -1,3 +1,5 @@
+
+
 Players = new Mongo.Collection("teams");
 Nodes = new Mongo.Collection("nodes");
 Items = new Mongo.Collection("items");
