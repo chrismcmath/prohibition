377fcd4d9656b11551fe75b3f39fe0ddfb7f60b2
Chris McMath <chrismcmath@gmail.com>
2015-06-05 06:39:29 +0800

    wip

--- client/templates/submit.js
+++ client/templates/submit.js
@@ -6,10 +6,20 @@ Template.submit.helpers({
          var i = 0;
          var list = [];
          Nodes.findOne(FlowRouter.getParam("nodeId")).items.forEach(function(item) {
-         list.push({name: item.name, i: i++});
+             list.push({name: item.name, i: i++});
          });
+         return list;
     }
 });
 
 Template.submit.events({
+    "submit": function(event, template){
+        // prevent default form submission handler : which is
+        // performing an HTTP POST request that will reload the page
+        event.preventDefault();
+        // construct the contact object that will be passed to the method
+        var boughtID = template.$('#bought-select').val();
+        var prohibitID = template.$('#prohibit-select').val();
+        CommitPurchase(boughtID, prohibitID, Nodes.findOne(FlowRouter.getParam("nodeId")), GetUserTeam());
+    }
 });
